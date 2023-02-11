52e5c837ab6bf448495e8aca9f062c0dd68730d9
zkk2157 <1066830769@qq.com>
2023-02-11 12:31:18 +0800

    feat:静态搜索页面

--- src/api/index.js
+++ src/api/index.js
@@ -4,18 +4,23 @@ import mockRequests from "@/api/mockRequest.js"
 //三级联动接口
 //  /api/product/getBaseCategoryList  get请求,无参数
 
-export const reqCategoryList = ()=>{
+export const reqCategoryList = () => {
     //发请求
-    return requests({url:"/product/getBaseCategoryList",method:"get"})
+    return requests({ url: "/product/getBaseCategoryList", method: "get" })
 }
 
-export const reqGetBannerList = ()=>{
+export const reqGetBannerList = () => {
 
     return mockRequests.get("/banner")
 }
 
 //获取floor 
-export const reqFloorList = ()=>{
+export const reqFloorList = () => {
 
     return mockRequests.get("/floor")
+}
+
+export const reqGetSearchInfo = (params) => {
+
+    return requests({ url: "/list", method: "post", data: params })
 }
--- src/store/Search/index.js
+++ src/store/Search/index.js
@@ -1,6 +1,23 @@
-const state = {}
-const mutations = {}
-const actions = {}
+import { reqGetSearchInfo } from "@/api"
+const state = {
+    searchList: {}
+}
+const mutations = {
+    GETSEARCHLIST(state, searchList) {
+        state.searchList = searchList
+    }
+}
+const actions = {
+    //获取search模块的数据
+    async getSearchList({ commit }, params = {}) {
+
+        let result = await reqGetSearchInfo(params)
+
+        if (result.code === 200) {
+            commit("GETSEARCHLIST", result.data)
+        }
+    }
+}
 const getters = {}
 
 export default {
